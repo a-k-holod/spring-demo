a0f8c5cea32a10921df4e4158a00a09f19a8acd8
Angelika <angelika.holod@gmail.com>
2023-01-31 00:57:38 +0100

    Simple json res, new class test.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,44 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+// import java.time.LocalDate;
+// import java.time.Month;
+// import java.util.List;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.example.demo.student.Student;
 
 
-@SpringBootApplication
-@RestController
-public class DemoApplication {
+// @SpringBootApplication
+// @RestController
+// public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DemoApplication.class, args);
+// 	}
 
-	@GetMapping
-	public List<String> hello(){
-		return List.of("hello", "world!"); 
-	}
+// 	// @GetMapping("/abc")
+// 	// public List<Student> hello(){
+// 	// 	return List.of(
+// 	// 		new Student(
+// 	// 			1L,
+// 	// 			"FunnyName",
+// 	// 			"test@test.com",
+// 	// 			LocalDate.of(1999, Month.JANUARY, 10),
+// 	// 			21
+// 	// 		)
+// 	// 	); 
+// 	// }
+// 	// public List<String> hello(){
+// 	// 	return List.of("hello", "world!"); 
+// 	// }
+// 	// public String hello(){
+// 	// 	return "hello world!"; 
+// 	// }
 
-}
+// }
